29bca0bc7fd25ad2cf42f1774c658bda1529a62d
Suresh Kumar K <mvksuresh.85@gmail.com>
2023-03-31 18:05:50 +0530

    InventoryWebApp added

---
+++ Model/CarDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryWebApp.Model
+{
+    public class CarDto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public string Make { get; set; }
+        [Required]
+        public string Year { get; set; }
+        [Required]
+        public double PurchasedAmount { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public string Colour { get; set; }
+
+        public double CurrentValue { get; set; }
+
+        public string Status { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
---
+++ Pages/CarController.cs
@@ -0,0 +1,94 @@
+﻿using InventoryWebApp.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace InventoryWebApp.Pages
+{
+    public class CarController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public CarController(IHttpClientFactory httpClientFactory) { 
+            _httpClientFactory = httpClientFactory;
+        }
+        // GET: HomeController
+        public async Task<ActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient("CarClient");
+            var response = await client.GetAsync($"api/Car");
+            response.EnsureSuccessStatusCode();
+            string carJson = await response.Content.ReadAsStringAsync();
+            CarDto carModel = JsonConvert.DeserializeObject<CarDto>(carJson);
+            return View(carModel);
+        }
+
+        // GET: HomeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HomeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HomeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
